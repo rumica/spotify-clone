fce4f9a4e815e79575aa4cc6bb4a304722b5dff4
rumica <rumeysavar2@gmail.com>
2022-03-17 01:49:33 +0300

    Activated volume section.

--- src/BottomBar/Player.js
+++ src/BottomBar/Player.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import { useAudio } from 'react-use';
 import { Icon } from 'Icons'
-import { Range, getTrackBackground } from "react-range";
+import CustomRange from 'CustomRange';
 import { secondsToTime } from 'utils';
 
-function Player() {
 
-    const STEP = 0.1;
-    const MIN = 0;
+function Player() {
 
     const [audio, state, controls, ref] = useAudio({
         src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
         autoPlay: true,
       });
     
+      const volumeIcon = useMemo(() =>{
+        if(state.volume === 0 || state.muted)
+        return 'volumeMute'
+
+        if(state.volume > 0 && state.volume < 0.33)
+        return 'volumeLow'
+
+        if(state.volume >= 0.33 && state.volume < 0.66)
+        return 'volumeNormal'
+
+        return 'volumeFull'
+      },[state.volume, state.muted])
 
   return ( 
     <div className='flex justify-between items-center h-full px-4'>
@@ -45,54 +55,49 @@ function Player() {
                 <div className='text-[0.688rem] text-white text-opacity-70'>
                     {secondsToTime(state?.time)}
                 </div>
-            <Range
-          values={[state?.time]}
-          step={STEP}
-          min={MIN}
-          max={state?.duration || 1}
-          onChange={(values) => controls.seek(values[0])}
-          renderTrack={({ props, children }) => (
-            <div
-              onMouseDown={props.onMouseDown}
-              onTouchStart={props.onTouchStart}
-              className='w-full h-7 flex group'
-              style={props.style}
-            >
-              <div
-                ref={props.ref}
-                className='h-1 w-full rounded-md self-center'
-                style={{
-                  background: getTrackBackground({
-                    values: [state?.time],
-                    colors: ["#1db954", "#535353"],
-                    min: MIN,
-                    max: state?.duration || 1
-                  }),
-                }}
-              >
-                {children}
-              </div>
-            </div>
-          )}
-          renderThumb={({ props, isDragged }) => (
-            <div
-              {...props}
-              className={`h-3 w-3 rounded-full bg-white ${!isDragged ? 'opacity-0' : ''} group-hover:opacity-100`}
-              style={{
-                ...props.style,
-                boxShadow: "0px 2px 4px 0 rgb(0 0 0 / 50%)"
-              }}
-            />
-            
-          )}
-        />
-        <div className='text-[0.688rem] text-white text-opacity-70'>
-            {secondsToTime(state?.duration)}
-        </div>
+                <div className='text-[0.688rem] text-white text-opacity-70'>
+                    {secondsToTime(state?.duration)}
+                </div>
+                <CustomRange 
+                step={0.1}
+                min={0}
+                max={state?.duration || 1}
+                value={state?.time}
+                onChange={value => controls.seek(value)}
+                />
             </div>
         </div>
-        <div className='min-w-[11.25rem] w-[30%] flex justify-end'>
-            sağ
+        <div className='min-w-[11.25rem] w-[30%] flex items-center justify-end'>
+            <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
+                    <Icon name="microphone" />
+                </button>
+                <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
+                    <Icon name="queue" />
+                </button>
+                <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
+                    <Icon name="device" />
+                </button>
+                <button 
+                className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'
+                onClick={controls[state.muted ? 'unmute' : 'mute']}
+                >
+                    <Icon name={volumeIcon} />
+                </button>
+                <div className='w-[5.813rem] max-w-full'>
+                    <CustomRange 
+                    step={0.01}
+                    min={0}
+                    max={1}
+                    value={state.muted ? 0 : state?.volume}
+                    onChange={value => {
+                        controls.unmute()
+                        controls.volume(value)
+                    }}
+                    />
+                </div>
+                <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
+                    <Icon name={"fullScreen"} />
+                </button>
         </div>
     </div>
   )
--- src/Icons.js
+++ src/Icons.js
@@ -108,25 +108,25 @@ const RepeatIcon = ({ size }) => {
 
 const MicrophoneIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M13.426 2.574a2.831 2.831 0 00-4.797 1.55l3.247 3.247a2.831 2.831 0 001.55-4.797zM10.5 8.118l-2.619-2.62A63303.13 63303.13 0 004.74 9.075L2.065 12.12a1.287 1.287 0 001.816 1.816l3.06-2.688 3.56-3.129zM7.12 4.094a4.331 4.331 0 114.786 4.786l-3.974 3.493-3.06 2.689a2.787 2.787 0 01-3.933-3.933l2.676-3.045 3.505-3.99z"/></svg>
+        <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path fill="currentColor" d="M13.426 2.574a2.831 2.831 0 00-4.797 1.55l3.247 3.247a2.831 2.831 0 001.55-4.797zM10.5 8.118l-2.619-2.62A63303.13 63303.13 0 004.74 9.075L2.065 12.12a1.287 1.287 0 001.816 1.816l3.06-2.688 3.56-3.129zM7.12 4.094a4.331 4.331 0 114.786 4.786l-3.974 3.493-3.06 2.689a2.787 2.787 0 01-3.933-3.933l2.676-3.045 3.505-3.99z"/></svg>
     )
 }
 
 const QueueIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 013.5 1h9a2.5 2.5 0 010 5h-9A2.5 2.5 0 011 3.5zm2.5-1a1 1 0 000 2h9a1 1 0 100-2h-9z"/></svg>
+        <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path fill="currentColor" d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 013.5 1h9a2.5 2.5 0 010 5h-9A2.5 2.5 0 011 3.5zm2.5-1a1 1 0 000 2h9a1 1 0 100-2h-9z"/></svg>
     )
 }
 
 const DeviceIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M6 2.75C6 1.784 6.784 1 7.75 1h6.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0114.25 15h-6.5A1.75 1.75 0 016 13.25V2.75zm1.75-.25a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h6.5a.25.25 0 00.25-.25V2.75a.25.25 0 00-.25-.25h-6.5zm-6 0a.25.25 0 00-.25.25v6.5c0 .138.112.25.25.25H4V11H1.75A1.75 1.75 0 010 9.25v-6.5C0 1.784.784 1 1.75 1H4v1.5H1.75zM4 15H2v-1.5h2V15z"/><path fill="currentColor" d="M13 10a2 2 0 11-4 0 2 2 0 014 0zm-1-5a1 1 0 11-2 0 1 1 0 012 0z"/></svg>
+        <svg role="img" height="16" width="16" class="Svg-sc-1bi12j5-0 hDgDGI" aria-label="Bir cihaza bağlan" viewBox="0 0 16 16"><path fill="currentColor" d="M6 2.75C6 1.784 6.784 1 7.75 1h6.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0114.25 15h-6.5A1.75 1.75 0 016 13.25V2.75zm1.75-.25a.25.25 0 00-.25.25v10.5c0 .138.112.25.25.25h6.5a.25.25 0 00.25-.25V2.75a.25.25 0 00-.25-.25h-6.5zm-6 0a.25.25 0 00-.25.25v6.5c0 .138.112.25.25.25H4V11H1.75A1.75 1.75 0 010 9.25v-6.5C0 1.784.784 1 1.75 1H4v1.5H1.75zM4 15H2v-1.5h2V15z"></path><path fill="currentColor" d="M13 10a2 2 0 11-4 0 2 2 0 014 0zm-1-5a1 1 0 11-2 0 1 1 0 012 0z"/></svg>
     )
 }
 
 const FullScreenIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M6.53 9.47a.75.75 0 010 1.06l-2.72 2.72h1.018a.75.75 0 010 1.5H1.25v-3.579a.75.75 0 011.5 0v1.018l2.72-2.72a.75.75 0 011.06 0zm2.94-2.94a.75.75 0 010-1.06l2.72-2.72h-1.018a.75.75 0 110-1.5h3.578v3.579a.75.75 0 01-1.5 0V3.81l-2.72 2.72a.75.75 0 01-1.06 0z"/></svg>
+        <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path fill="currentColor" d="M6.53 9.47a.75.75 0 010 1.06l-2.72 2.72h1.018a.75.75 0 010 1.5H1.25v-3.579a.75.75 0 011.5 0v1.018l2.72-2.72a.75.75 0 011.06 0zm2.94-2.94a.75.75 0 010-1.06l2.72-2.72h-1.018a.75.75 0 110-1.5h3.578v3.579a.75.75 0 01-1.5 0V3.81l-2.72 2.72a.75.75 0 01-1.06 0z"/></svg>
     )
 }
 
@@ -144,25 +144,25 @@ const PictureInPictureIcon = ({ size }) => {
 
 const VolumeMuteIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"/><path fill="currentColor" d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"/></svg>
+        <svg role="presentation" height="16" width="16" aria-label="Ses kapalı" id="volume-icon" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path fill="currentColor" d="M13.86 5.47a.75.75 0 00-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 008.8 6.53L10.269 8l-1.47 1.47a.75.75 0 101.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 001.06-1.06L12.39 8l1.47-1.47a.75.75 0 000-1.06z"/><path fill="currentColor" d="M10.116 1.5A.75.75 0 008.991.85l-6.925 4a3.642 3.642 0 00-1.33 4.967 3.639 3.639 0 001.33 1.332l6.925 4a.75.75 0 001.125-.649v-1.906a4.73 4.73 0 01-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 01-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"/></svg>
     )
 }
 
 const VolumeNormalIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 000-8.474v1.65a2.999 2.999 0 010 5.175v1.649z"/></svg>
+        <svg role="presentation" height="16" width="16" aria-label="Ses seviyesi orta" id="volume-icon" viewBox="0 0 16 16"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 000-8.474v1.65a2.999 2.999 0 010 5.175v1.649z"/></svg>
     )
 }
 
 const VolumeLowIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/></svg>
+        <svg role="presentation" height="16" width="16" aria-label="Ses seviyesi düşük" id="volume-icon" viewBox="0 0 16 16"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/></svg>
     )
 }
 
 const VolumeFullIcon = ({ size }) => {
     return (
-        <svg role="img" height={size} width={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/><path fill="currentColor" d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"/></svg>
+        <svg role="presentation" height="16" width="16" aria-label="Ses seviyesi yüksek" id="volume-icon" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path fill="currentColor" d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"/><path fill="currentColor" d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"/></svg>
     )
 }
 
